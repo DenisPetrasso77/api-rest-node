b46f5c262d7e3b9444d5a9ca3bd2a8a41637a705
Denis <denispetrasso77@gmail.com>
2025-07-17 04:07:44 -0300

    Add .nav-example

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
+import "dotenv/config";
 import express from "express";
+
 const app = express();
-
-
 
 app.get('/', (req,res) => {
     res.json("Bienvenido a nuestra API REST !!!!")
@@ -14,6 +14,6 @@ app.use('/api', productsRouter);
 import notFound from "./src/middlewares/not-found.js";
 app.use(notFound);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
--- src/models/firebase.js
+++ src/models/firebase.js
@@ -6,12 +6,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDeSYFuKLbOIy-INK5pxllmXAIckBIaBdk",
-  authDomain: "api-rest-node-285c0.firebaseapp.com",
-  projectId: "api-rest-node-285c0",
-  storageBucket: "api-rest-node-285c0.firebasestorage.app",
-  messagingSenderId: "807286286357",
-  appId: "1:807286286357:web:4730569d9c0d58b8aa461c"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.IREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
